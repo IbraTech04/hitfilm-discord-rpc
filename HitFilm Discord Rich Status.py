665276b903f198d81a9e747ba16b01fcce1e0713
Ibrahim Chehab <chehab.ibrahim@outlook.com>
2021-07-02 13:23:21 -0400

    Added comments (for some reason)

--- HitFilm Discord Rich Status.py
+++ HitFilm Discord Rich Status.py
@@ -1,34 +1,33 @@
-from pypresence import Presence
+from pypresence import Presence #Library Importing
 import time
 import time
 from time import mktime
 import psutil    
 import win32gui
-w=win32gui
+w=win32gui #Initializing win32gui
 
-client_id = '860354715886682112'  
-RPC = Presence(client_id) 
-print("RPC connection successful.")
+client_id = '860354715886682112'  #Client ID for HitFilm Express
+RPC = Presence(client_id) #Setting up RPC connection
 
-RPC.connect()
+RPC.connect() #Connecting to RPC
 
-while True:
-    if ("HitFilmExpress.exe" in (p.name() for p in psutil.process_iter())):
-        start_time = mktime(time.localtime())
-        while ("HitFilmExpress.exe" in (p.name() for p in psutil.process_iter())):
+while True: #Main Loop
+    if ("HitFilmExpress.exe" in (p.name() for p in psutil.process_iter())): #Checks if the program is runing
+        start_time = mktime(time.localtime()) #Sets start time
+        while ("HitFilmExpress.exe" in (p.name() for p in psutil.process_iter())): #While the user is still in HitFilm Express
             status = ""
             state = ""
-            if (".hfp" in w.GetWindowText (w.GetForegroundWindow())):
+            if (".hfp" in w.GetWindowText (w.GetForegroundWindow())): ##If the user is in the HitFilm window
                 state = "In a File" 
                 status = w.GetWindowText (w.GetForegroundWindow()).split("-",1)[0]
-            elif ( w.GetWindowText (w.GetForegroundWindow()) == "HitFilm Express"):
+            elif ( w.GetWindowText (w.GetForegroundWindow()) == "HitFilm Express"): ##If the user is on the homescreen
                 state = "Idling"
                 status = "Homescreen"
-            elif ("Untitled Project" in w.GetWindowText (w.GetForegroundWindow())):
+            elif ("Untitled Project" in w.GetWindowText (w.GetForegroundWindow())): #If the user is in an untitled project
                 state = "In a File" 
                 status = "Untitled Project"
-            else:
+            else: ##If the user isn't even in the app in the first place
                 state = "Idling"
                 status = "Not even in the app lmao"
-            (RPC.update(state=status, details=state, large_image="hitfilm", small_image="fxhome", small_text = "FXHome", large_text="HitFilm", start=start_time))
-    RPC.clear()
+            (RPC.update(state=status, details=state, large_image="hitfilm", small_image="fxhome", small_text = "FXHome", large_text="HitFilm", start=start_time)) #Sending data
+    RPC.clear() #If the while loop above does not (Meaning the user isn't in the app) run, clear the RPC (therefore removing the discord rich status)
