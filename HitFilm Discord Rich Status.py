904242d274b872984b04201f6b4f8d532db9a082
Ibrahim Chehab <chehab.ibrahim@outlook.com>
2021-07-02 13:53:29 -0400

    Added idling detection

--- HitFilm Discord Rich Status.py
+++ HitFilm Discord Rich Status.py
@@ -4,18 +4,25 @@ import time
 from time import mktime
 import psutil    
 import win32gui
+
 w=win32gui #Initializing win32gui
-
-client_id = 'yeah this is not going on github sorry...'  #Client ID for HitFilm Express
+client_id = '860354715886682112'  #Client ID for HitFilm Express
 RPC = Presence(client_id) #Setting up RPC connection
-
+counter = 0
 RPC.connect() #Connecting to RPC
-
+mouse = win32gui.GetCursorPos()
 while True: #Main Loop
-    print("its working")
     if ("HitFilmExpress.exe" in (p.name() for p in psutil.process_iter())): #Checks if the program is runing
         start_time = mktime(time.localtime()) #Sets start time
         while ("HitFilmExpress.exe" in (p.name() for p in psutil.process_iter())): #While the user is still in HitFilm Express
+            prevMouse = mouse
+            mouse = win32gui.GetCursorPos()
+            if (prevMouse == mouse):
+                print("here")
+                counter = counter + 1
+                print(counter)
+            else:
+                counter = 0
             status = ""
             state = ""
             if (".hfp" in w.GetWindowText (w.GetForegroundWindow())): ##If the user is in the HitFilm window
@@ -27,8 +34,17 @@ while True: #Main Loop
             elif ("Untitled Project" in w.GetWindowText (w.GetForegroundWindow())): #If the user is in an untitled project
                 state = "In a File" 
                 status = "Untitled Project"
+            elif ("Open Project" in w.GetWindowText (w.GetForegroundWindow())): #If the user is in an untitled project
+                state = "Opening a Project"
+                status = "Loading files"
+
+            elif ("Relink Files" in w.GetWindowText (w.GetForegroundWindow())): #If the user is in an untitled project
+                state = "Relinking files"
+                status = "Probably deleted something"
             else: ##If the user isn't even in the app in the first place
                 state = "Idling"
                 status = "Not even in the app lmao"
+            if (counter >= 100):
+                status = "Idling"
             (RPC.update(state=status, details=state, large_image="hitfilm", small_image="fxhome", small_text = "FXHome", large_text="HitFilm", start=start_time)) #Sending data
     RPC.clear() #If the while loop above does not (Meaning the user isn't in the app) run, clear the RPC (therefore removing the discord rich status)
